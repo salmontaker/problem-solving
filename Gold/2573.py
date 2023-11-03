e16edca5010efaa97512986ba5a92ebdf3b2a270
Seungwon Lee <seungwon.dev@gmail.com>
2023-11-03 20:47:55 +0900

    2573: 빙산
    https://www.acmicpc.net/problem/2573
    
    효율성 개선

--- Gold/2573.py
+++ Gold/2573.py
@@ -4,33 +4,31 @@ from collections import deque
 input = sys.stdin.readline
 
 
-def melt(y, x):
-    cnt = 0
-    for dy, dx in delta:
-        Y, X = y + dy, x + dx
-        if -1 < Y < N and -1 < X < M and area[Y][X] == 0:
-            cnt += 1
-
-    return max(area[y][x] - cnt, 0)
-
-
 def bfs(y, x):
     queue = deque([(y, x)])
     visited[y][x] = 1
 
     while queue:
         y, x = queue.popleft()
+        cnt = 0
+
         for dy, dx in delta:
             Y, X = y + dy, x + dx
-            if -1 < Y < N and -1 < X < M and area[Y][X] != 0 and not visited[Y][X]:
-                queue.append((Y, X))
-                visited[Y][X] = 1
+            if -1 < Y < N and -1 < X < M and not visited[Y][X]:
+                if area[Y][X] != 0:
+                    queue.append((Y, X))
+                    visited[Y][X] = 1
+                else:
+                    cnt += 1
+
+        if cnt:
+            melt.append((y, x, max(area[y][x] - cnt, 0)))
 
 
 N, M = map(int, input().split())
 area = [list(map(int, input().split())) for _ in range(N)]
 delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-temp = deque()
+melt = deque()
 year = 0
 
 
@@ -38,7 +36,6 @@ while True:
     visited = [[0] * M for _ in range(N)]
     cnt = 0
 
-    # 빙산 덩어리 개수
     for y in range(N):
         for x in range(M):
             if area[y][x] != 0 and not visited[y][x]:
@@ -53,15 +50,8 @@ while True:
         print(0)
         break
 
-    # 빙산이 녹아내리는 높이 계산
-    for y in range(N):
-        for x in range(M):
-            if area[y][x] != 0:
-                temp.append((y, x, melt(y, x)))
-
-    # 배열에 데이터 반영
-    while temp:
-        y, x, m = temp.popleft()
-        area[y][x] = m
+    while melt:
+        y, x, n = melt.popleft()
+        area[y][x] = n
 
     year += 1
