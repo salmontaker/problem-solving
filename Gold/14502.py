9b158ca85aba32e5bef329858ab0233657745c46
Seungwon Lee <seungwon.dev@gmail.com>
2024-02-22 16:36:08 +0900

    refactor 14502: 연구소
    https://www.acmicpc.net/problem/14502

--- Gold/14502.py
+++ Gold/14502.py
@@ -1,55 +1,43 @@
-import sys, copy
+import sys
+from itertools import combinations
 from collections import deque
 
 input = sys.stdin.readline
 
-
-def make_wall(n):
-    if n == 3:
-        bfs()
-        return
-
-    for y in range(N):
-        for x in range(M):
-            if AREA[y][x] == "0":
-                AREA[y][x] = "1"
-                make_wall(n + 1)
-                AREA[y][x] = "0"
-
-
-def bfs():
-    global answer
-
-    queue = deque()
-    area = copy.deepcopy(AREA)
-
-    for y in range(N):
-        for x in range(M):
-            if area[y][x] == "2":
-                queue.append((y, x))
-
-    while queue:
-        y, x = queue.popleft()
-
-        for dy, dx in DIR:
-            Y, X = y + dy, x + dx
-            if -1 < Y < N and -1 < X < M and area[Y][X] == "0":
-                queue.append((Y, X))
-                area[Y][X] = "2"
-
-    cnt = 0
-    for y in range(N):
-        for x in range(M):
-            if area[y][x] == "0":
-                cnt += 1
-
-    answer = max(answer, cnt)
-
-
 N, M = map(int, input().split())
-AREA = [input().strip().split() for _ in range(N)]
-DIR = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+A = [[*map(int, input().split())] for _ in range(N)]
+EMPTY = []
+VIRUS = []
+
+for y in range(N):
+    for x in range(M):
+        if A[y][x] == 0:
+            EMPTY.append((y, x))
+        elif A[y][x] == 2:
+            VIRUS.append((y, x))
+
 answer = 0
 
-make_wall(0)
+for walls in combinations(EMPTY, 3):
+    Q = deque()
+    for y, x in VIRUS:
+        Q.append((y, x))
+
+    R = [row[:] for row in A]
+    for y, x in walls:
+        R[y][x] = 1
+
+    safe = len(EMPTY) - 3
+
+    while Q:
+        y, x = Q.popleft()
+        for dy, dx in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+            Y, X = y + dy, x + dx
+            if -1 < Y < N and -1 < X < M and R[Y][X] == 0:
+                Q.append((Y, X))
+                R[Y][X] = 2
+                safe -= 1
+
+    answer = max(answer, safe)
+
 print(answer)
